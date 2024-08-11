ffc41cbb31a02781fa2dbc159e2d12c793edd973
Farhan <144435177+toufiqfarhan0@users.noreply.github.com>
2024-08-11 10:13:31 +0530

    nextAuth in merchant and user app

--- apps/user-app/app/api/user/route.ts
+++ apps/user-app/app/api/user/route.ts
@@ -1,15 +1,17 @@
-import client from "@repo/db/client";
+import { getServerSession } from "next-auth"
 import { NextResponse } from "next/server";
-
+import { authOptions } from "../../lib/auth";
 
 export const GET = async () => {
-  await client.user.create({
-    data: {
-      email: "user@example.com",
-      name: "user",
-    },
-  });
-  return NextResponse.json({
-    message: "data successfully gone to database",
-  });
-};
+    const session = await getServerSession(authOptions);
+    if (session.user) {
+        return NextResponse.json({
+            user: session.user
+        })
+    }
+    return NextResponse.json({
+        message: "You are not logged in"
+    }, {
+        status: 403
+    })
+}
